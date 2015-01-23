1004233df3fac701e78d7ff0f0feda39b7a005b8
bhayes724 <bhayes724@gmail.com>
2015-01-23 11:22:04 -0800

    fixed folder issues

---
+++ CelebrityNames.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Sorts a list of names by birthdate
+ * 
+ * @author Roger Jaffe, Bryan Hayes
+ * @version 2015-01-19
+ */
+public class CelebrityNames
+{
+    public static final String FILE_NAME = "CelebrityData.txt";
+    public static void main(String args[]) throws IOException
+    { 
+        Scanner sf = new Scanner(new File(FILE_NAME));
+
+        int maxIndx = -1; //-1 so when we increment below, the first index is 0
+        String text[] = new String[1000]; //to be safe, declare more than we need
+
+        while(sf.hasNext( ))
+        { 
+            maxIndx++;
+            text[maxIndx] = sf.nextLine( ); 
+        } 
+        //maxIndx is now the highest index of text[], = -1 if no text lines 
+        sf.close( ); //We opened a file above, so close it when finished.
+
+        String reversedName[] = new String[maxIndx + 1];
+        for (int j = 0; j <= maxIndx; j++)
+        { 
+            Scanner sc = new Scanner( text[j] );
+            String firstName = sc.next( );
+            String middleName = sc.next( );
+            String lastName = sc.next( );
+            String birthday = sc.next( );
+            reversedName[j] = birthday + " " + lastName + ", " + firstName + " " + middleName + "-- " + birthday;
+        }
+        
+        Arrays.sort(reversedName);
+        String sortedName[] = new String[maxIndx + 1];
+        
+        for (int j = 0; j <= maxIndx; j++)
+        { 
+            Scanner sc = new Scanner( reversedName[j] );
+            String birthday = sc.next( );
+            String lastName = sc.next( );
+            String firstName = sc.next( );
+            String middleName = sc.next( );
+            sortedName[j] = lastName + " " + firstName + " " + middleName + birthday;
+        }
+        
+        FileWriter fw = new FileWriter("Output1.out");
+        PrintWriter output = new PrintWriter(fw);
+        
+        for (int j =0; j <= maxIndx; j++)
+        { 
+            System.out.println(sortedName[j]);
+            output.println(sortedName[j]);
+        } 
+        output.close();
+        fw.close();
+    }
+}
